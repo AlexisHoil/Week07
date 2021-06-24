00e3bdd2a72fc8bdecae8dfe218d887957c08766
AlexisHoil <1909090@upy.edu.mx>
2021-06-24 04:47:07 -0500

    chapter6ex04

--- chapter6ex04.c
+++ chapter6ex04.c
@@ -1,6 +1,5 @@
 /*  Given an amount of money (less than $1.00), compute the number
 of quarters, dimes, nickels, and pennies needed. */
-
 #include <stdio.h>
 
 char line[100];
@@ -9,12 +8,13 @@ float rest;
 float rest2;
 float rest3;
 float dim1;
+float pen;
 int check;
 int mod;
 int quar;
 int dim;
 int nickels;
-int pen;
+
 
 int main(void) {
 
@@ -24,26 +24,30 @@ int main(void) {
   check= mone*100;
   mod= check % 10; 
 
-if ( mod ==.5){
+if ( mod == 5 && check !=55 && check != 5 && check != 15){
   quar = mone / 0.25;
   rest = mone - (quar * 0.25);
   dim1 = rest  / 0.10;
   nickels = 0;
+  rest2= 
   pen = 0;
-printf("the $ %.2f contains %d cuarters, %1.0f dimes, %d nickels, %d pennies\n ", mone, quar, dim1, nickels, pen);
+printf("the $ %.2f contains %d cuarters, %1.0f dimes, %d nickels, %1.0f pennies\n ", mone, quar, dim1, nickels, pen);
 
 }
  else{
   quar = mone / 0.25;
   rest = mone - (quar * 0.25);
   dim = rest  / 0.10;
-  rest2 = rest * 1.0  - (dim * 0.10);
+  rest2 = rest  - (dim * 0.10);
   nickels = rest2 / 0.05;
-  rest3 = rest2 * 1.0 - (nickels * 0.05);
+  rest3 = rest2 - (nickels * 0.05);
   pen = rest3 / 0.01;
-printf("the $ %.2f contains %d cuarters, %d dimes, %d nickels, %d pennies\n ", mone, quar, dim, nickels, pen);
+
+printf("the $ %.2f contains %d cuarters, %d dimes, %d nickels, %1.0f pennies\n ", mone, quar, dim, nickels, pen);
+
  
  }
+
 
  return 0;
 }
